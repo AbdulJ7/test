6bcdef76733e6c245dd604ba2639ae4b07f5e20e
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-03-05 07:30:25 +0000

    Create test.sh
    ugi

---
+++ test.sh
@@ -0,0 +1,46 @@
+
+#!/usr/bin/bash
+self_update() {
+  # store the current dir
+CUR_DIR=$(pwd)
+
+# Let the person running the script know what's going on.
+echo -e "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
+
+# Find all git repositories and update it to the master latest revision
+for i in $(find -L $HOME/.local/share/bash-calc -name ".git" | cut -c 1-); do
+    echo "";
+    echo -e "\033[33m"+$i+"\033[0m";
+
+    # We have to go to the .git parent directory to call the pull command
+    cd "$i";
+    cd ..;
+
+    # git checkout master;
+    git remote prune origin;
+    git fetch origin;
+    git pull;
+
+    # lets get back to the CUR_DIR
+    cd $CUR_DIR
+done
+
+echo -e "\n\033[32mComplete!\033[0m\n"
+}
+
+main() { 
+   htop
+}
+
+for arg in "$@"; do
+  if [[ "$arg" = -u ]] || [[ "$arg" = --upgrade ]]; then
+    ARG_INSTALL_REQUIREMENTS=true
+  fi
+done
+
+
+if [[ "$ARG_INSTALL_REQUIREMENTS" = true ]]; then
+  self_update
+fi
+
+main
